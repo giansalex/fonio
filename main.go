edd6b0240b0f5caa24362e9d7a0e0976dfb154ec
Giancarlos Salas <giansalex@gmail.com>
2018-04-29 21:13:48 -0500

    add first sample

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/rjeczalik/notify"
+)
+
+func exampleWatch() {
+	// Make the channel buffered to ensure no event is dropped. Notify will drop
+	// an event if the receiver is not able to keep up the sending pace.
+	c := make(chan notify.EventInfo, 1)
+
+	// Set up a watchpoint listening on events within current working directory.
+	// Dispatch each create and remove events separately to c.
+	if err := notify.Watch(".", c, notify.Create, notify.Remove); err != nil {
+		log.Fatal(err)
+	}
+	defer notify.Stop(c)
+
+	// Block until an event is received.
+	ei := <-c
+	log.Println("Got event:", ei.Path())
+}
+
+func main() {
+	exampleWatch()
 }
